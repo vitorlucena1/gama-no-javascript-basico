2492a00942e1c8a4d31ac5cd6f42d3f90efdf894
Vitor Campos <vlfc.jogos@gmail.com>
2022-05-18 11:50:07 -0300

    Aula 2

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('Javascrip carregado')
+
+function validaCPF(cpf){
+    return false;
+}
+
+function validacao() {
+    console.log('Iniciando validação CPF');
+
+    var cpf = document.getElementById('cpf_digitado').value;
+    
+    var resultadoValidacao = validaCPF(cpf);
+
+    if(resultadoValidacao) {
+        document.getElementById('success').style.display = 'block';
+    }
+    else {
+        document.getElementById('error').style.display = 'block';
+    }
+}
